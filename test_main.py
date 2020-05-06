068da3eb94836909d3fff01022ceb0d4807ac0e1
Paul Alexander <paulalexander23@live.co.uk>
2020-05-06 11:05:36 +0100

    Improve tests.

--- test_main.py
+++ test_main.py
@@ -1,68 +1,52 @@
 #usr/local/bin/python3
 
 import subprocess
-
-def test_shellScript():
-    bashCommand = "./generate-pbs.sh test.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    f  = open("test.pbs","r")
-    actual = f.read()
-    f  = open("testExpected.pbs","r")
-    expected = f.read()
-    assert actual == expected
-
+import re
 
 def test_pythonScript():
-    bashCommand = "python3 generate-pbs.py test.csv"
+    bashCommand = "python3 generate-pbs-script.py test.csv"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     f  = open("test.pbs","r")
     actual = f.read()
     f  = open("testExpected.pbs","r")
-    expected = f.read()
-    assert actual == expected
-
-
-def test_memoryArgument():
-    bashCommand = "python3 generate-pbs.py -m 32 test.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    f  = open("test.pbs","r")
-    actual = f.read()
-    f  = open("testExpectedMemory.pbs","r")
-    expected = f.read()
-    assert actual == expected
-
-
-def test_wiblArgument():
-    bashCommand = "python3 generate-pbs.py --wibl1 test.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    f  = open("test.pbs","r")
-    actual = f.read()
-    f  = open("testExpectedWIBL1.pbs","r")
     expected = f.read()
     assert actual == expected
 
 
 def test_walltimeArgument():
-    bashCommand = "python3 generate-pbs.py -t 71:30:00 test.csv"
+    bashCommand = "python3 generate-pbs-script.py -t 12:34:56 test.csv"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     f  = open("test.pbs","r")
-    actual = f.read()
-    f  = open("testExpectedWalltime.pbs","r")
-    expected = f.read()
+    for line in f:
+        if "walltime" in line:
+            actual = line
+    expected = "#PBS -l walltime=12:34:56\n"
     assert actual == expected
 
 
-def test_runScript():
-    bashCommand = "python3 generate-pbs.py -r test.csv"
+def test_ncpusArgument():
+    bashCommand = "python3 generate-pbs-script.py -c 8 test.csv"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     f  = open("test.pbs","r")
-    actual = f.read()
-    f  = open("testExpected.pbs","r")
-    expected = f.read()
+    for line in f:
+        for section in line.split(":"):
+            if "ncpus" in section:
+                actual = section
+    expected = "ncpus=8"
+    assert actual == expected
+
+
+def test_memoryArgument():
+    bashCommand = "python3 generate-pbs-script.py -m 32 test.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    f  = open("test.pbs","r")
+    for line in f:
+        for section in line.split(":"):
+            if "mem" in section:
+                actual = section
+    expected = "mem=32gb\n"
     assert actual == expected
